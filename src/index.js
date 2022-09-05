00f0417663072ef1b5d69894e182d095e4d0b57d
Michael <mikahado@gmail.com>
2022-09-04 23:35:07 -0400

    mini project complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target.newTaskDescription.value)
+    //original id was 'new-task-description'. error with hyphen. works when i removed hyphens. why!?
+    form.reset()
+  })
+})
+
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' X '
+  p.textContent = todo
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+
+}
+
